f06e7578dcd5ed7e79fe81a03fefbf0fcab0ce6c
Gordon Hsu <ghsu477@aucklanduni.ac.nz>
2018-05-28 10:29:20 +0800

    Initial commit

---
+++ Assets/Emergency.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Make sure you get rid of the namespace that is automatically generated here.
+class Emergency
+{
+    //List all the columns of your table here in the same format as below.
+    public string EmergencyID { get; set; }
+    public string EmergencyName { get; set; }
+    public float x { get; set; }
+    public float y { get; set; }
+    public float z { get; set; }
+
+}
